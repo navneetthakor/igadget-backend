f2bbf58102a9337c6562d8808142f53a8e0c2631
navneetthakor <tnavneer8628@gmail.com>
2023-08-20 20:02:44 +0530

    product model first stage ready

--- model/Product.js
+++ model/Product.js
@@ -1,1 +1,41 @@
-//it is done letter
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+productSchema = Schema({
+    image: [{
+        type:Buffer,
+        require: true
+    }],
+    title:{
+        type: String,
+        require: true,
+    },
+    description:{
+        overview: {
+            company: {type: String},
+            model: {type: String},
+            height: {type: Number},
+            width: {type: String},
+            other: [{
+                type: String,
+                require: true
+            }]
+        },
+        brief: {type: String},
+        require: true
+    },
+    dummyPrice:{
+        type: Number,
+    },
+    price:{
+        type: Number,
+        require: true
+    },
+    rating: [{
+        userId: { type: mongoose.Types.ObjectId },
+        rating: { type: Number },
+        comment: { type: String }
+    }]
+})
+
+module.exports = mongoose.model('Product', productSchema);
